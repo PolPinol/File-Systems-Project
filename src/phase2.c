94f0c0611562f0ff7ddaa8d3129c82e4e2774a27
Pol Pinol Castuera <pol.pinol@mongodb.com>
2023-04-21 12:49:15 +0200

    Read first directories

---
+++ src/phase2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "../include/phase2.h"
+#include "../include/ext2.h"
+#include "../include/fat16.h"
+
+
+void phase2(const char *fileName) {
+    if (is_ext2(fileName)) {
+        tree_ext2(fileName);
+    } else if (is_fat16(fileName)) {
+        // tree_fat16(fileName);
+    } else {
+        printf("Unknown\n");
+    }
+}

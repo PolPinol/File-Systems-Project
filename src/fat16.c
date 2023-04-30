015ffdf6ec0e5c4ce2170fe97aef1de2d9637643
Pol Pinol Castuera <pol.pinol@mongodb.com>
2023-04-30 22:32:26 +0200

    Implement read of the first directories

--- src/fat16.c
+++ src/fat16.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,88 +6,122 @@
 #include "../include/fat16.h"
 #include "../include/util.h"
 
+fat16_boot_sector bs;
 
 int is_fat16(const char *filename) {
-    FILE* fp = fopen(filename, "rb");
-    if (!fp) {
-        printf("Error opening file\n");
-        exit(EXIT_FAILURE);
-    }
+  FILE *fp = fopen(filename, "rb");
+  if (!fp) {
+    printf("Error opening file\n");
+    exit(EXIT_FAILURE);
+  }
 
-    uint8_t buffer[FAT16_SECTOR_SIZE];
-    fread(buffer, sizeof(buffer), 1, fp);
+  fat16_boot_sector bs;
+  fseek(fp, 0, SEEK_SET);
+  fread(&bs, sizeof(fat16_boot_sector), 1, fp);
 
-    // Create a pointer to the partition table within the buffer
-    fat16_partition_table* partition_table = (fat16_partition_table*)&buffer[0x1BE];
-
-    // Iterate over the partition table entries and print their properties
-    fat16_partition_entry* partition_entry = NULL;
-    for (int i = 0; i < 4; i++) {
-        partition_entry = &partition_table->partition_table[i];
-        if (partition_entry->start_sector_lba == 1) {
-            break;
-        }
-    }
-
-    if (partition_entry == NULL) {
-        fclose(fp);
-        return FALSE;
-    }
-
-    fat16_boot_sector* bs = (fat16_boot_sector*)&buffer[0];
-    if (strncmp(bs->fs_type, "FAT16", 5) != 0) {
-        fclose(fp);
-        return FALSE;
-    }
-
+  if (strncmp(bs.fs_type, "FAT16", 5) != 0) {
     fclose(fp);
+    return FALSE;
+  }
 
-    return TRUE;
+  fclose(fp);
+
+  return TRUE;
 }
 
 void metadata_fat16(const char *filename) {
-    FILE *fp = fopen(filename, "rb");
-    if (!fp) {
-        printf("Error opening file\n");
-        exit(EXIT_FAILURE);
-    }
+  FILE *fp = fopen(filename, "rb");
+  if (!fp) {
+    printf("Error opening file\n");
+    exit(EXIT_FAILURE);
+  }
 
-    uint8_t buffer[FAT16_SECTOR_SIZE];
-    fread(buffer, sizeof(buffer), 1, fp);
+  fat16_boot_sector bs;
+  fseek(fp, 0, SEEK_SET);
+  fread(&bs, sizeof(fat16_boot_sector), 1, fp);
 
-    fat16_boot_sector* bs = (fat16_boot_sector*)&buffer[0];
+  printf("\n------ Filesystem Information ------\n\n");
+  printf("Filesystem: FAT16\n\n");
 
-    printf("\n------ Filesystem Information ------\n\n");
-    printf("Filesystem: FAT16\n\n");
+  printf("System name: %s\n", bs.oem);
+  printf("Sector size: %d\n", bs.bytes_per_sector);
+  printf("Sectors per cluster: %d\n", bs.sectors_per_cluster);
+  printf("Reserved sectors: %d\n", bs.reserved_sectors);
+  printf("# of FATs: %d\n", bs.number_of_fats);
+  printf("Max root entries: %d\n", bs.root_dir_entries);
+  printf("Sectors per FAT: %d\n", bs.sectors_per_fat);
+  printf("Label: %.*s\n", 11, bs.volume_label);
 
-    printf("System name: %s\n", bs->oem);
-    printf("Sector size: %d\n", bs->bytes_per_sector);
-    printf("Sectors per cluster: %d\n", bs->sectors_per_cluster);
-    printf("Reserved sectors: %d\n", bs->reserved_sectors);
-    printf("# of FATs: %d\n", bs->number_of_fats);
-    printf("Max root entries: %d\n", bs->root_dir_entries);
-    printf("Sectors per FAT: %d\n", bs->sectors_per_fat);
-    printf("Label: %.*s\n", 11, bs->volume_label);
+  printf("\n");
 
-    printf("\n");
-    
-    fclose(fp);
+  fclose(fp);
 }
 
-// TODO next phases
 /*
-// Create a pointer to the partition table within the buffer
-fat16_partition_table* partition_table = (fat16_partition_table*)&buffer[0x1BE];
+uint32_t FirstDataSector;
+FirstDataSector = bs.reserved_sectors +
+                (bs.number_of_fats * bs.sectors_per_fat) +
+                root_directory_sectors;*/
 
-// Iterate over the partition table entries and print their properties
-for (int i = 0; i < 4; i++) {
-    fat16_partition_entry* partition_entry = &partition_table->partition_table[i];
+void tree_fat16(const char *filename) {
+  FILE *fp = fopen(filename, "rb");
+  if (!fp) {
+    printf("Error opening file\n");
+    exit(EXIT_FAILURE);
+  }
 
-    printf("Partition %d:\n", i+1);
-    printf("Boot Indicator: 0x%02X\n", partition_entry->boot_indicator);
-    printf("Start CHS: %d-%d-%d\n", partition_entry->start_cylinder, partition_entry->start_head, partition_entry->start_sector);
-    printf("Partition Type: 0x%02X\n", partition_entry->partition_type);
-    printf("End CHS: %d-%d-%d\n", partition_entry->end_cylinder, partition_entry->end_head, partition_entry->end_sector);
-    printf("Start LBA: %d\n", partition_entry->start_sector_lba);
-    printf("Total Sectors: %d\n", partition_entry->total_sectors);
-}*/
+  fat16_boot_sector bs;
+  fseek(fp, 0, SEEK_SET);
+  fread(&bs, sizeof(fat16_boot_sector), 1, fp);
+
+  uint32_t first_root_sector =
+      bs.reserved_sectors + (bs.number_of_fats * bs.sectors_per_fat);
+  uint32_t root_directory_sectors =
+      ((bs.root_dir_entries * 32) + (bs.bytes_per_sector - 1)) /
+      FAT16_SECTOR_SIZE;
+
+  uint32_t current_sector = first_root_sector;
+  for (uint32_t i = 0; i < root_directory_sectors; i++) {
+    fseek(fp, current_sector * bs.bytes_per_sector, SEEK_SET);
+    for (uint16_t j = 0; j < (bs.bytes_per_sector / sizeof(fat16_dir_entry));
+         j++) {
+      fat16_dir_entry entry;
+      fread(&entry, sizeof(fat16_dir_entry), 1, fp);
+
+      // long name - not supported
+      if (entry.filename[6] == '~' && entry.filename[7] == '1') {
+        continue;
+      }
+
+      // skip "." and ".."
+      if (entry.filename[0] == 0x2E) {
+        continue;
+      }
+
+      if (entry.attributes == ATTR_DIRECTORY) {
+        char filename[15];
+        int size_filename = 0;
+        for (int k = 0; k < 8 && entry.filename[k] != 0x20; k++) {
+          filename[size_filename] = tolower(entry.filename[k]);
+          size_filename++;
+        }
+
+        for (int k = 8; k < 11 && entry.filename[k] != 0x20; k++) {
+          if (k == 8) {
+            filename[size_filename] = '.';
+            size_filename++;
+          }
+
+          filename[size_filename] = tolower(entry.filename[k]);
+          size_filename++;
+        }
+
+        filename[size_filename] = '\0';
+        printf("%s\n", filename);
+      }
+    }
+    current_sector++;
+  }
+
+  fclose(fp);
+}
--- src/phase2.c
+++ src/phase2.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-#include "../include/phase2.h"
 #include "../include/ext2.h"
 #include "../include/fat16.h"
-
+#include "../include/phase2.h"
 
 void phase2(const char *fileName) {
-    if (is_ext2(fileName)) {
-        tree_ext2(fileName);
-    } else if (is_fat16(fileName)) {
-        // tree_fat16(fileName);
-    } else {
-        printf("Unknown\n");
-    }
+  if (is_ext2(fileName)) {
+    tree_ext2(fileName);
+  } else if (is_fat16(fileName)) {
+    tree_fat16(fileName);
+  } else {
+    printf("Unknown\n");
+  }
 }

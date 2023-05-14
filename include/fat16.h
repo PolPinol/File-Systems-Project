c47efdde8610327669cf362b76c1d9aa9d8de035
Angel <angelgg02@gmail.com>
2023-05-14 20:09:28 +0200

    Fixed is_fat function

--- include/fat16.h
+++ include/fat16.h
@@ -30,6 +30,36 @@ typedef struct {
 } __attribute((packed)) fat16_boot_sector;
 
 typedef struct {
+  uint8_t jmp[3];
+  char oem[8];
+  uint16_t bytes_per_sector;
+  uint8_t sectors_per_cluster;
+  uint16_t reserved_sectors;
+  uint8_t number_of_fats;
+  uint16_t root_dir_entries;
+  uint16_t total_sectors_small;
+  uint8_t media_descriptor;
+  uint16_t sectors_per_fat;
+  uint16_t sectors_per_track;
+  uint16_t number_of_heads;
+  uint32_t hidden_sectors;
+  uint32_t total_sectors_long;
+  uint32_t sectors_per_fat_long;
+  uint16_t flags;
+  uint16_t version;
+  uint32_t root_cluster;
+  uint16_t fsinfo;
+  uint16_t backup_boot_sector;
+  uint8_t reserved[12];
+  uint8_t drive_number;
+  uint8_t current_head;
+  uint8_t boot_signature;
+  uint32_t volume_id;
+  char volume_label[11];
+  char fs_type[8];
+} __attribute((packed)) fat32_boot_sector;
+
+typedef struct {
   uint8_t filename[11];
   uint8_t attributes;
   uint8_t reserved;
